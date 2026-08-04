893e2b408b236fb9fcd192d504382fc8b752f179
Ram Mane <rammane1171@gmail.com>
2026-08-04 19:41:09 +0530

    Time: 6 ms (22.06%), Space: 46.2 MB (16.7%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,17 +3,13 @@ class Solution {
         if (x < 0) {
             return false;
         }
-
         String s = String.valueOf(x);
-
         int left = 0;
         int right = s.length() - 1;
-
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
-
             left++;
             right--;
         }
