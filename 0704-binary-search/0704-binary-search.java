3f1a8da721423615d00a8d6cefe829b7f62e7d2a
Ram Mane <rammane1171@gmail.com>
2026-09-19 11:35:21 +0530

    Time: 0 ms (100%), Space: 48.5 MB (13.94%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,50 +1,21 @@
-import java.util.Scanner;
 
 public class Solution {
 
     public int search(int[] nums, int target) {
-
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            }
-
-            else {
-                right = mid - 1;
-            }
+       int low = 0;
+       int high = nums.length-1;
+       while(low<=high){
+        int mid = (low + high) /2;
+        if(nums[mid] == target){
+            return mid;
+        }else if(nums[mid]<target){
+            low = mid+1;
+        }else{
+            high = mid -1;
         }
-
-        return -1;
-    }
-
-    public static void main(String[] args) {
-
-        int[] nums = {-1, 0, 3, 5, 9, 12};
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter target: ");
-        int target = sc.nextInt();
-
-        Solution obj = new Solution();
-
-        int result = obj.search(nums, target);
-
-        System.out.println("Index = " + result);
-
-        sc.close();
+       }
+       return -1;
     }
 }
-
 
  
